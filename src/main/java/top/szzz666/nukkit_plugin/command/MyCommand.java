c7f4837b63f83035e1393129b49828d6829b2381
Szzz666 <unconfigured@null.spigotmc.org>
2025-05-21 15:52:37 +0800

    refactor(command): 优化 MyCommand 类
    - 移除了未使用的 options 数组
    - 修改了命令描述为插件名称命令
    - 删除了冗余的权限设置和参数配置

--- src/main/java/top/szzz666/nukkit_plugin/command/MyCommand.java
+++ src/main/java/top/szzz666/nukkit_plugin/command/MyCommand.java
@@ -3,7 +3,6 @@ package top.szzz666.nukkit_plugin.command;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.command.ConsoleCommandSender;
-import cn.nukkit.command.data.CommandParameter;
 
 import static top.szzz666.nukkit_plugin.Main.ec;
 import static top.szzz666.nukkit_plugin.Main.plugin;
@@ -11,14 +10,9 @@ import static top.szzz666.nukkit_plugin.config.MyConfig.initConfig;
 
 
 public class MyCommand extends Command {
-    String[] options = {"reload"};
+
     public MyCommand() {
-        super(ec.getString("command"), "重新加载" + plugin.getName() + "插件配置");
-        this.setPermission(plugin.getName() + ".command");
-        this.commandParameters.clear();
-        this.commandParameters.put("default", new CommandParameter[]{
-                CommandParameter.newEnum("opt", options)
-        });
+        super(ec.getString("command"), plugin.getName() + "命令");
     }
 
     @Override
